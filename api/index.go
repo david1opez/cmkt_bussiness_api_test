7ac519e8defdf661fff555d53344cb40fd39c2bb
david1opez <david1opez@outlook.com>
2023-11-08 15:43:46 -0600

    Remove dev env file from production route

--- api/index.go
+++ api/index.go
@@ -5,21 +5,10 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
-
 	"campusmarket/mongo"
 )
-
-func loadEnv() {
-	if err := godotenv.Load(".env.dev"); err != nil {
-        fmt.Println("Error loading .env file")
-        return
-    }
-}
  
 func Handler(w http.ResponseWriter, r *http.Request) {
-	loadEnv()
-
 	user := os.Getenv("MONGO_USER")
 	password := os.Getenv("MONGO_PASSWORD")
 	host := os.Getenv("MONGO_HOST")
