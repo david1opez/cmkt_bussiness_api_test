467fbbb3365af6e7eaf97bdf6619d2ca25128a34
david1opez <david1opez@outlook.com>
2023-11-12 18:47:15 -0600

    Update authorization logic

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,27 +1,14 @@
 package middleware
 
 import (
-	"crypto/sha256"
-	"fmt"
 	"net/http"
 	"os"
 )
 
 func Authorize(auth string) int {
-	authE := sha256.New()
-	authE.Write([]byte(auth))
-
 	key := os.Getenv("MAGIC_WORD")
-	keyE := sha256.New()
-	keyE.Write([]byte(key))
 
-	fmt.Println(key)
-	fmt.Println(keyE.Sum(nil))
-
-	fmt.Println(auth)
-	fmt.Println(authE.Sum(nil))
-
-	if authE != keyE {
+	if auth != key {
 		return http.StatusUnauthorized
 	} else {
 		return http.StatusAccepted
