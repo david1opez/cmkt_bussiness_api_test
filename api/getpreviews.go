3e53667681f175cafdb6463f82c8a30cd64cdfc8
david1opez <david1opez@outlook.com>
2023-11-12 18:20:54 -0600

    Add getpreviews route

---
+++ api/getpreviews.go
@@ -0,0 +1,37 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"campusmarket/middleware"
+)
+
+type GetPreviewsRequest struct {
+	Page int32 `bson:"page,omitempty"`
+}
+
+func GetPreviews(w http.ResponseWriter, r *http.Request) {
+	if middleware.Authorize(r.Header.Get("X-CMKT-Authorization")) == http.StatusUnauthorized {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+	
+	if r.Method == "GET" {
+		// Get Request Data
+		decoder := json.NewDecoder(r.Body)
+		var reqBody GetPreviewsRequest
+
+		err := decoder.Decode(&reqBody)
+
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Println(err)
+			panic(err)
+		}
+
+		// Validate Data
+
+		// Get Previews
+	}
+}
